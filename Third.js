f81ed8f098f1caee03e48e6168b7e6df80164899
Student <cjethva149>
2021-12-31 09:24:44 +0530

    Third completed

---
+++ Third.js
@@ -0,0 +1,13 @@
+var sum=(a,b)=>{
+    return a+b;
+}
+var div=(a,b)=>{
+    return a+b;
+}
+var multi=(a,b)=>{
+    return a+b;
+}
+
+module.exports.add=sum;
+module.exports.divi=div;
+module.exports.mul=multi;
---
+++ fourth.js
@@ -0,0 +1,5 @@
+var test = require('./Third');
+
+console.log(test.add(10,20));
+console.log(test.divi(20,10));
+console.log(test.mul(10,2));
